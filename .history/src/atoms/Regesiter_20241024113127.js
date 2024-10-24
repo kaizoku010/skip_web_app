47829819caf6ed81e9dfaf79cc2b003cc4adb720
mRodneyMo <motokads@gmail.com>
2024-10-24 11:37:50 +0300

    cleaning up

---
+++ .history/src/atoms/Regesiter_20241024113127.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState,useContext } from 'react';
+import "./EventForm.css"
+import EventsContext from '../logic/DataPoint';
+import { Link, useParams } from 'react-router-dom';
+import Login from './Login';
+import MXForm from './MXForm';
+import "./regesiter.css"
+import {
+  ArrowLeftOutlined 
+} from '@ant-design/icons';
+
+import Quotes from "./Quotes.js"
+
+
+
+function Regesiter() {
+  const [isRegistrationSuccessful, setIsRegistrationSuccessful] = useState(false);
+
+  return (
+    <div className='evt-form'>
+         <div className='inner-form'>
+        {/* Conditionally render success message */}
+        {isRegistrationSuccessful && (
+          <div className='show-me-we-are-good'>Registration was successful!</div>
+          
+        )}
+
+        {/* Form Side */}
+        <div style={{display:"flex", flexDirection:"column"}}>
+            <div className='create_mobile' style={{textAlign:"left", paddingLeft:"2rem"}}>
+           <p><span><ArrowLeftOutlined style={{marginRight: "",}} /></span>Back Home</p>
+            <h3>Create Account </h3>
+            <p>Start by providing a few details about yourself and a profile image</p>
+           
+            </div>
+          <div className='form-side'>
+          <div className='form-side-holder'>         
+            <MXForm/> 
+          </div>
+        </div> 
+        </div>
+      
+
+        
+      </div>
+   
+   <div className='form-left-side register_page'>
+    <span className='spacer'></span>
+    <div className='qoutes_layout'>
+      <Quotes/>
+    </div>
+   </div>
+      
+    </div>
+  );
+}
+
+
+export default Regesiter
