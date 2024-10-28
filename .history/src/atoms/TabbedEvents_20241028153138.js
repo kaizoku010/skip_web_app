bc2aadd64d61905b9d1248735312766cca65ca88
mRodneyMo <motokads@gmail.com>
2024-10-28 15:48:00 +0300

    tabbed very well 1.0

---
+++ .history/src/atoms/TabbedEvents_20241028153138.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from "react";
+import { Radio, Tabs } from "antd";
+import CurrentEvent from "./CurrentEvent";
+import EventsAttended from "./EventsAttended";
+import { AuthContext } from "../logic/AuthContext"; // Make sure the path to AuthProvider is correct
+
+function TabbedEvents() {
+  const { user, events } = useContext(AuthContext); // Get the user from AuthContext
+
+  return (
+    <div className="current_event_mobile">
+      <div className="mdx-tabs">
+        <Tabs
+          defaultActiveKey="1"
+          type=""
+          size="large"
+          items={[
+            {
+              label: "Active Event",
+              key: "1",
+              children: <CurrentEvent events={events} user={user} />, // Render FirstTabContent in the first tab
+            },
+            {
+              label: "Events Attended",
+              key: "2",
+              children: <EventsAttended />, // Render SecondTabContent in the second tab
+            },
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TabbedEvents;
